2ad65af6ef8cf5bfab385fb2464cc011a590f83b
mfahadarif85 <mfahadarif@gmail.com>
2022-12-28 11:49:32 +0500

    step-ts-sec10

---
+++ stephen-ts/sort/build/Sorter.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Sorter = void 0;
+// interface Sortable {
+//   length: number;
+//   compare(leftIndex: number, rightIndex: number): boolean;
+//   swap(leftIndex: number, rightIndex: number): void;
+// }
+class Sorter {
+    sort() {
+        const { length } = this;
+        // bubble sort
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
+                }
+            }
+        }
+    }
+}
+exports.Sorter = Sorter;
