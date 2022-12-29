771fb77dd7bc470f48056eaff16920e60f5d83b6
mfahadarif85 <mfahadarif@gmail.com>
2022-12-29 12:10:56 +0500

    steph-ts-sec11-102

--- stephen-ts/stats/src/index.ts
+++ stephen-ts/stats/src/index.ts
@@ -1,17 +1,20 @@
-import fs from "fs";
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+import { CsvFileReader } from "./CsvFileReader";
+
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+//enum - enumeration
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
+
 let manUnitedWins = 0;
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
