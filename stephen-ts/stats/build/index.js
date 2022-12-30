2799c97014b9beaf5110fb7ec046bf54a004462f
mfahadarif85 <mfahadarif@gmail.com>
2022-12-30 12:31:30 +0500

    steph-ts-sec11-120

--- stephen-ts/stats/build/index.js
+++ stephen-ts/stats/build/index.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchResult_1 = require("./MatchResult");
+const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
-const reader = new MatchReader_1.MatchReader("football.csv");
-reader.read();
+// create an object that satisfies the 'datareader' interface
+const csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+// create an instance of matchreader and pass in something satisfying the datareader interface
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
--- stephen-ts/stats/src/index.ts
+++ stephen-ts/stats/src/index.ts
@@ -1,10 +1,16 @@
 import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-const reader = new MatchReader("football.csv");
-reader.read();
+
+// create an object that satisfies the 'datareader' interface
+const csvFileReader = new CsvFileReader("football.csv");
+
+// create an instance of matchreader and pass in something satisfying the datareader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
