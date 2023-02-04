ea5693d5b29640600f28069ca7417377bedf12d9
mfahadarif85 <mfahadarif@gmail.com>
2023-02-04 11:51:21 +0500

    steph-ts-138

--- stephen-ts/web/src/index.ts
+++ stephen-ts/web/src/index.ts
@@ -1,1 +1,10 @@
-console.log("hi there");
+import { User } from "./models/user";
+
+const user = new User({ name: "myname", age: 20 });
+
+user.set({ name: "newName", age: 99 });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
+
+user.set({ name: "newName", age: 99 });
